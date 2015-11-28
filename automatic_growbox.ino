a6c70cfbd87b0734a091344c6d6c4de96c31f76e
dznet <kuchin.ea@gmail.com>
2015-11-28 15:56:48 +0200

    Automatic GrowBox v.0.2

--- automatic_growbox.ino
+++ automatic_growbox.ino
@@ -1,44 +1,86 @@
-unsigned long last_time;
+// Automatic GrowBox by dzNET
+
+
+#include "DHT.h"
+#define DHTPIN 0
+#define DHTTYPE DHT22
+
 unsigned long diff_time;
 
 const int H_MIN  = 700;
 const int H_WARN = 400;
+const int air_time = 3000;
+const int water_time = 6000;
+const long interval = 14400000;
 
-int w_pump   = 12;
-int warning  = 11;
-int comfort  = 10;
-int sensor   = A0;
-int humidity = 0;
+int w_pump = 12;
+int warning = 11;
+int comfort = 10;
+int dhtErr = 9;
+int hot_air = 8;
+int cold_air = 7;
+int inPin = A0;
+int num = 0;
 
-void setup()
-{
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
   pinMode(w_pump, OUTPUT);
   pinMode(warning, OUTPUT);
   pinMode(comfort, OUTPUT);
-  pinMode(sensor, INPUT);
+  pinMode(dhtErr, OUTPUT);
+  pinMode(hot_air, OUTPUT);
+  pinMode(cold_air, OUTPUT);
+  pinMode(inPin, INPUT);
+  dht.begin();
 }
 
-void loop()
-{
-  humidity = analogRead(sensor);
+void loop() {
+  airPump();  
+  waterPump();
+}
+
+void waterPump() {
+  num = analogRead(inPin);
   
-  if(humidity >= H_WARN) {
-    diff_time = millis() - last_time;
-    if(diff_time > 14400000) {
-      last_time = millis();
+  if(num >= H_WARN) {;
+    if(millis() - diff_time > interval) {
+      diff_time = millis();
       digitalWrite(w_pump, HIGH);
-      delay(30000);
+      delay(3000);
       digitalWrite(w_pump, LOW);
     }
-  } 
-  else {
+  }else {
     digitalWrite(comfort, HIGH);
     delay(1000);
     digitalWrite(comfort, LOW);
   }
-  if(humidity >= H_MIN) {
+  if(num >= H_MIN) {
     digitalWrite(warning, HIGH);
-    delay(30000);
+    delay(1000);
     digitalWrite(warning, LOW);
+  }
+}
+
+void airPump() {
+  float h   = dht.readHumidity();
+  float t   = dht.readTemperature();
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  if (isnan(h) || isnan(t)) {
+    digitalWrite(dhtErr, HIGH);
+    delay(1000);
+    digitalWrite(dhtErr, LOW);
+    return;
+  }  
+  if (t < 20.00){
+    digitalWrite(hot_air, HIGH);
+    delay(3000);
+    digitalWrite(hot_air, LOW);
+  }
+  if (t > 24.00) {
+    digitalWrite(cold_air, HIGH);
+    delay(3000);
+    digitalWrite(cold_air, LOW);
   }
 }
